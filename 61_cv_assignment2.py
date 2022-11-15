87328e1e881043b9efb0fc09d3cfd3312cdad639
dnyanada <71203168+dnyanada02@users.noreply.github.com>
2022-11-16 00:07:57 +0530

    Update 61_cv_assignment2.py

--- 61_cv_assignment2.py
+++ 61_cv_assignment2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1apw3W4RLtfUccno12ZWk2LzZh4xrxpQU
 """
 
-! pip install streamlit
+!pip install streamlit
 
 !pip install streamlit_drawable_canvas
 
